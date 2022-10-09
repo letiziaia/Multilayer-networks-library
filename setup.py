65a28c088ddcb19a7cee9f8f0170d3e9486448cd
letiziaia <letizia852@gmail.com>
2022-10-09 16:32:31 +0300

    add requires

--- setup.py
+++ setup.py
@@ -11,5 +11,8 @@ setup(
     url="https://bitbucket.org/bolozna/multilayer-networks-library",
     author="Mikko Kivelä",
     author_email="mikko.kivela@iki.fi",
+    install_requires=[
+        "numpy", "scipy", "pybind11", "Cython", "networkx", "matplotlib<=3.3.0"
+    ],
     packages=["pymnet", "pymnet.tests","pymnet.visuals","pymnet.visuals.drawbackends","pymnet.isomorphisms","pymnet.graphlets"]
 )
